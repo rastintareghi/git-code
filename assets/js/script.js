25a28c97aec14974295d698dcd3036a01cef2038
Matin Mousavi <matinmousavi5049@gmail.com>
2024-10-12 18:43:56 +0330

    fix: splice data after every spin

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ let modalWrapper = document.querySelector(".modal-wrapper");
 let modalBtn = document.querySelector(".modal-btn");
 let orderNumberTd = document.querySelector(".orderNumberTd");
 let fullNameTd = document.querySelector(".fullNameTd");
-
 let data = null;
 let orderNumber = null;
 let fullName = null;
@@ -19,17 +18,15 @@ fileInput.addEventListener("change", async () => {
     if (allowedExtensions.includes(fileExtension)) {
       formLabel.classList.add("hidden");
       button.classList.remove("hidden");
-
       try {
         let response = await uploadFile(file);
-        if (Array.isArray(response.data)) {
-          data = response.data;
-          selectRandomData();
+        if (response && Array.isArray(response.data)) {
+          data = [...response.data];
+          console.log("Data loaded:", data);
         } else {
           orderNumber = response.data["شماره سفارش"];
           fullName = response.data["نام (صورتحساب)"] + " " + response.data["نام خانوادگی (صورتحساب)"];
         }
-
       } catch (error) {
         console.error("Error during file upload:", error);
         showToast("در ارسال فایل خطایی رخ داد.", "error");
@@ -44,7 +41,6 @@ fileInput.addEventListener("change", async () => {
 const uploadFile = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-
   try {
     let response = await fetch("https://ap.sbsuntech.ir/api", {
       method: "POST",
@@ -54,47 +50,58 @@ const uploadFile = async (file) => {
     if (!response.ok) {
       throw new Error(`خطا در ارسال فایل: ${response.statusText}`);
     }
-
     let result = await response.json();
     return result;
   } catch (error) {
     throw error;
   }
 };
+
 function selectRandomData() {
-  if (data) {
+  if (data && data.length > 0) {
     let randomIndex = Math.floor(Math.random() * data.length);
     orderNumber = data[randomIndex]["شماره سفارش"];
     fullName = data[randomIndex]["نام (صورتحساب)"] + " " + data[randomIndex]["نام خانوادگی (صورتحساب)"];
+    data.splice(randomIndex, 1);
+  }
+  if (data && data.length === 0) {
+    button.classList.add("hidden");
   }
 }
 
 button.addEventListener("click", spinWheel);
 
 function spinWheel(evt) {
-  let spin = Math.round(Math.random() * 5220);
-  wheel.style.setProperty("transition", "ease 2.5s");
-  wheel.style.transform = 'rotate(' + spin + 'deg)';
+  if (data && data.length > 0) {
+    button.disabled = true;
+    button.style.cursor = "not-allowed";
+    let spin = Math.round(Math.random() * 5220);
+    wheel.style.setProperty("transition", "ease 2.5s");
+    wheel.style.transform = 'rotate(' + spin + 'deg)';
+    setTimeout(() => {
+      if (orderNumber) {
+        orderNumberTd.innerText = orderNumber;
+        fullNameTd.innerText = fullName;
+        modalWrapper.classList.remove("d-none");
+      } else {
+        modalWrapper.classList.add("d-none");
+        console.log("شماره سفارش موجود نیست.");
+      }
+      button.classList.remove("hidden");
+      button.disabled = false;
+      button.style.cursor = "pointer";
+    }, 2500);
 
-  setTimeout(() => {
-    if (orderNumber) {
-      orderNumberTd.innerText = orderNumber;
-      fullNameTd.innerText = fullName;
-      modalWrapper.classList.remove("d-none");
-    } else {
-      modalWrapper.classList.add("d-none");
-      console.log("شماره سفارش موجود نیست.");
-    }
-    button.classList.remove("hidden");
-
-  }, 2500);
-  selectRandomData();
+    selectRandomData();
+  } else {
+    showToast("دیگر اسمی برای قرعه‌کشی باقی نمانده است", "error");
+  }
 }
 
 modalBtn.addEventListener("click", () => {
   modalWrapper.classList.add("d-none");
-  orderNumber = null; 
-  fullName = null; 
+  orderNumber = null;
+  fullName = null;
 });
 
 function showToast(message, type) {
@@ -103,9 +110,7 @@ function showToast(message, type) {
     duration: 3000,
     gravity: "top",
     position: "right",
-    backgroundColor: type === "error" 
-      ? "red" 
-      : "green",
+    backgroundColor: type === "error" ? "red" : "green",
     close: true
   }).showToast();
 }
