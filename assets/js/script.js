aee7620c11eb626a7e2a042043076488ba4f244e
rastin tareghi <rastintareghi@gmail.com>
2025-03-11 15:42:37 +0330

    test project

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,3 +118,26 @@ function showToast(message, type) {
 const userName='rastin'
 const userNames='rastin'
 
+// git pull
+// git branch -r
+// # title
+// <P>desc</P>
+// _ _ _ 
+//   `` for highlight
+// ```
+// for coding
+// ```
+///////
+// ``` for language
+// for coding
+// ```
+// for lists
+// ol
+// 1.
+// 2.
+// n.
+// ul 
+// -
+//////
+//  a tag [for title] (for link)
+// for a img ![]()
